bfa634df9850b7308967e982fea4adecfa570a0a
git-sunao <sunaosugiyama@gmail.com>
2024-03-18 12:32:15 -0400

    update for pixwin in theory

--- python/fastnc_interface.py
+++ python/fastnc_interface.py
@@ -1,11 +1,12 @@
 '''
 Author     : Sunao Sugiyama
-Last edit  : 2024/03/14 14:46:48
+Last edit  : 2024/03/18 12:22:06
 
 
 TODO:
 - IA
 - allow for user-defined t1 and t2 binning to be consistent with treecorr output. (currently the output is on FFT grid of fastnc)
+- phi binning
 '''
 from cosmosis.datablock import option_section, names
 import numpy as np
@@ -26,6 +27,15 @@ def get_sample_sombinations(options):
     o = get_string_array_1d(options, option_section, "sample_combinations")
     o = [tuple(x.split(',')) for x in o]
     return o
+
+def get_healpix_window_function(nside):
+    import healpy as hp
+    from scipy.interpolate import interp1d
+    w = hp.sphtfunc.pixwin(1024)
+    l = np.arange(3*nside)
+    fnc = interp1d(l, w, kind='linear', bounds_error=False, fill_value=(1,0))
+    window = lambda l1, l2, l3: fnc(l1) * fnc(l2) * fnc(l3)
+    return window
 
 def setup(options):
     """
@@ -73,6 +83,10 @@ def setup(options):
         print('Setting detault dlnt to None.')
         config['dlnt'] = None
 
+    # pixel window function
+    config['use-pixwin'] = options.get_bool(option_section, "use-pixwin", default = False)
+    config['nside'] = options.get_int(option_section, "nside", default = 1024)
+
     return config
 
 def execute(block, config):
@@ -115,10 +129,22 @@ def execute(block, config):
     nc = config['fastnc']
     sctname = "ggg"
 
+    # pixel window function
+    # multiplied on bispectrum in Fourier space
+    # This is only needed when compared with simulation
+    # with finite resolution.
+    # Now, this assumes the resolution is determined by the Nside
+    # os healpix.
+    if config['use-pixwin']:
+        print('Applying pixel window function.')
+        window = get_healpix_window_function(config['nside'])
+    else:
+        window = None
+
     for sample_combination in config['sample-combinations']:
         print('calculating sample_combination:', sample_combination)
         # set bispectrum
-        nc.set_bispectrum(bs, sample_combinations=[sample_combination])
+        nc.set_bispectrum(bs, sample_combinations=[sample_combination], window=window)
     
         # compute 3PCF
         Gamma = nc.Gamma(
