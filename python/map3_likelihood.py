7dad01b57bb054828f802b68fe005ec0f9d0ed19
Sunao Sugiyama <sunao.sugyama@gmail.com>
2024-05-01 12:56:46 +0900

    updated

--- python/map3_likelihood.py
+++ python/map3_likelihood.py
@@ -36,47 +36,8 @@ def execute(block, config):
     icov = data.get_inverse_covariance()
     chi2 = np.matmul(diff, np.matmul(icov, diff))
 
-    # Get bins for natural component predictions:
-    section_nc = 'natural_components'
-    phi = block[section_nc, 'phi']
-    t1  = block[section_nc, 't1'] * 180*60/np.pi # in arcmin
-    t2  = block[section_nc, 't2'] * 180*60/np.pi # in arcmin
-    logr_bin_size = np.log(t2[1])-np.log(t2[0])
-    phi_bin_size = phi[1]-phi[0]
-    phi, t1, t2 = np.meshgrid(phi, t1, t2, indexing='ij')
-
-    zbin_combinations = config["sample-combinations"]
-    num_zbin_combinations = config["num_sample"]
-    num_aperture_combinations = config["num_aperture"]
-    num_all_combinations = num_zbin_combinations*num_aperture_combinations
-
-    y = np.zeros(num_all_combinations)
-
-    for i in range(num_zbin_combinations):
-
-        print(zbin_combinations[i])
-        print(zbin_combinations)
-        print(num_zbin_combinations)
-        if isinstance(zbin_combinations[i], tuple):
-            name = '_'.join(zbin_combinations[i])
-        else:
-            name = str(zbin_combinations[i])
-        gamma = block[section_nc, f'real-bin_{name}'] + 1j*block[section_nc, f'imag-bin_{name}']
-
-        y_temp = np.real(calculateMap3(gamma, t2, t1, phi, logr_bin_size, phi_bin_size, filters))
-        #y_temp = calculateMap3(gamma, t1, t2, phi, logr_bin_size, phi_bin_size, filters)
-        block['map3', f'map3-bin_{name}'] = y_temp
-        y[i*num_aperture_combinations:(i+1)*num_aperture_combinations] = y_temp 
-    block['map3', 'filters'] = filters
-
-    # likelihood
-    w = y-config['y_obs']
-    chi2 = np.matmul(w,np.matmul(config['inv_cov'],w))
-    block[names.likelihoods, name_likelihood] = -0.5 * np.real(chi2)
-    print(y)
-    print(config['y_obs'])
-    #np.save("theory_19April_map3_new_zbins_los-trial3", y)
-    np.save("theory_29Apr_cosmogrid_params_no_IA", y)
+    block[names.likelihoods, 'map3_like'] = -0.5*chi2
+    block[names.data_vector, 'map3_chi2'] = chi2
 
     return 0
 
