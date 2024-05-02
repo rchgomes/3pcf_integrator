488de2e55e2a680277da44402a4f90b27ad41037
Rafael Gomes <gchgomes@ist-06825.apn.wlan.private.upenn.edu>
2024-05-02 12:44:11 -0400

    update

--- setup-3pcf-path.sh
+++ setup-3pcf-path.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 # Setup path of the project
 
-# source cosmosis configuration
-source cosmosis-configure
-
 # Common setup for every environment
-here=$(dirname "$(pwd)/${BASH_SOURCE[0]}")
+#here=$(dirname "$(pwd)/${BASH_SOURCE[0]}")
 # path to this project directory, used in the ini file.
-export COSMOSIS_3PCF_INTEGRATOR=$here
+export COSMOSIS_3PCF_INTEGRATOR=/Users/gchgomes/3pcf_integrator
 # path to the python module.
-export PYTHONPATH=$here/python:$PYTHONPATH
+#export PYTHONPATH=$here/python:$PYTHONPATH
 
 # environment dependent setups
 # path to cosmosis-standard-library, used in the ini file.
-export COSMOSIS_STD_LIB=${here}/../cosmosis-standard-library/
+#export COSMOSIS_STD_LIB=/Users/gchgomes/PycharmProjects/bispectrum_integrator/new_structure/cosmosis-standard-library/
+export COSMOSIS_STD_LIB=/Users/gchgomes/PycharmProjects/bispectrum_integrator/cosmosis-standard-library/
 
-unset here
+#unset here
