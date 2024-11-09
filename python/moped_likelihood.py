91bb30d53ee70f652978c7e974152f345d598fad
Rafael Gomes <rafaelchgomes@gmail.com>
2024-11-09 13:27:10 -0500

    solve bug

--- python/moped_likelihood.py
+++ python/moped_likelihood.py
@@ -61,7 +61,7 @@ def execute(block, config):
     full_cov  = scipy.linalg.block_diag(*full_cov)
 
     # Transform data and theory
-    if compressed_2pt_data == None:
+    config['compressed_2pt_data'] == None:
         transformed_data = np.dot(config["transform_matrix"], full_data)
     else:
         transformed_data = config["compressed_2pt_data"]
