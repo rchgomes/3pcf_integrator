616a93d3effdd8ecb6f11eba60c874896060a7fe
git-sunao <sunaosugiyama@gmail.com>
2025-03-06 13:21:01 -0800

    added public chain download script

---
+++ public_chains/download_chains.sh
@@ -0,0 +1,11 @@
+# Chains public at https://des.ncsa.illinois.edu/releases/y3a2/Y3key-products
+
+# LCDM
+## Fiducial LCDM Cosmic Shear (MagLim)
+wget http://desdr-server.ncsa.illinois.edu/despublic/y3a2_files/chains/chain_1x2pt_lcdm_SR_maglim.txt
+## LCDM ‘Optimized’ Cosmic Shear (MagLim)
+wget http://desdr-server.ncsa.illinois.edu/despublic/y3a2_files/chains/chain_1x2pt_lcdm_SR_maglim_opt.txt
+
+# wCDM
+## Fiducial wCDM Cosmic Shear (MagLim)
+wget http://desdr-server.ncsa.illinois.edu/despublic/y3a2_files/chains/chain_1x2pt_wcdm_SR_maglim.txt
