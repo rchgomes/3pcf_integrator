3b65f7abee250e0637693586f16956a59a5e3cc6
Rafael Gomes <rafaelchgomes@gmail.com>
2025-10-06 13:00:05 -0400

    emu train files

---
+++ config/emulator-train-tatt/generate_ini_script.py
@@ -0,0 +1,24 @@
+import itertools
+import os
+
+def generate_ini_files(template_content):
+    #ddE_variants = ["dEd", "Edd", "dEE", "EdE", "EEd", "EEE"]
+    ddE_variants = ["ddE"]
+    range_variants = ["2000to3000", "3000to4000", "4000to5000"]
+    #range_variants = ["1000to2000"]
+
+    for ddE_var, range_var in itertools.product(ddE_variants, range_variants):
+        new_content = template_content.replace("ddE", ddE_var)
+        new_content = new_content.replace("1000to2000", range_var)
+
+        output_filename = f"generate_training_set_{ddE_var}_{range_var}.ini"
+        with open(output_filename, "w") as f:
+            f.write(new_content)
+        print(f"Generated {output_filename}")
+
+# Read the template content from the provided file
+with open("generate_training_set_ddE_1000to2000.ini", "r") as f:
+    template_ini_content = f.read()
+
+generate_ini_files(template_ini_content)
+
