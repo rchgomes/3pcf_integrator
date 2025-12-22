6ea739ee34d06a57dd89d44105eb4c6c08124acf
Rafael Gomes <rafaelchgomes@gmail.com>
2025-12-22 15:30:51 -0300

    emu train scripts

---
+++ script/generated_scripts_december/emu_EEd_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+#SBATCH -A des 
+#SBATCH -C cpu 
+#SBATCH -q regular 
+#SBATCH -t 24:00:00 
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=128
+#SBATCH --ntasks=1
+
+cd /global/cfs/cdirs/des/rchgoms1/3pcf_integrator
+module load python
+conda activate csis3pcf
+
+source setup-3pcf-path.sh
+srun cosmosis config/emulator-train-tatt/december-2025/generate_training_set_EEd_900to1800.ini
